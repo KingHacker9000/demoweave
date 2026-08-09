c368aa9861ade6f517476512516b192a86a16661
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2026-08-09 18:10:18 +0400

    feat(core): export analyzer API

---
+++ packages/core/src/index.ts
@@ -0,0 +1,3 @@
+export { analyzeProject } from './analyzer.js';
+export { ProjectProfileSchema, SurfaceSchema, SurfaceTypeSchema } from './types.js';
+export type { ProjectProfile, Surface, SurfaceType } from './types.js';
